f917fbc9ac727279011dfc860b2f7a06dc9af1e1
ym2976 <ym2976@columbia.edu>
2025-11-02 23:39:48 -0500

    chore: remove broken submodule and delete evals directory

--- evals/audio_metrics/torch_patch.py
+++
@@ -1,6 +0,0 @@
-import torch
-_real_torch_load = torch.load
-def _full_unpickle_load(f, *args, **kwargs):
-    kwargs['weights_only'] = False
-    return _real_torch_load(f, *args, **kwargs)
-torch.load = _full_unpickle_load
